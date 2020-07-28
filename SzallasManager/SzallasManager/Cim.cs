ea6453a73849409ee681be2028dca6d708c307dd
Mutz Dezső <mutz.dezso@yahoo.com>
2020-07-28 09:22:01 +0200

    OOP str1

---
+++ SzallasManager/SzallasManager/Cim.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace SzallasManager
+{
+    struct Cim
+    {
+        short irsz;
+        string varos;
+        string utca;
+        short hsz;
+
+        public short Irsz 
+        {
+            get => irsz;
+            set
+            {
+                if (value >= 1000 && value <= 9999)
+                {
+                    irsz = value;
+                }
+                else
+                {
+                    throw new FormatException("Azirsz 100 és 9999 között lehet!");
+                }
+            }
+        }
+        public string Varos
+        { 
+            get => varos;
+            set
+            {
+                if (value.Length != 0)
+                {
+                    varos = value;
+                }
+                else
+                {
+                    throw new FormatException("A város nem lehet üres!");
+                }
+            }
+        }
+        public string Utca 
+        { 
+            get => utca;
+            set
+            {
+                if (value.Length == 8)
+                {
+                    utca = value;
+                }
+                else
+                {
+                    throw new FormatException("Az utca nem lehet üres!");
+                }
+            }
+        }
+        public short Hsz 
+        {
+            get => hsz;
+            set
+            {
+                if (value > 0)
+                {
+                    hsz = value;
+                }
+                else
+                {
+                    throw new FormatException("Az utca nem lehet üres!");
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ SzallasManager/SzallasManager/Szallashely.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzallasManager
+{
+    enum Szallasfajta
+    {
+        Üzleti,
+        Gyógy,
+        Sport,
+        Egyéb
+    }
+    abstract class Szallashely
+    {
+        Cim cim;
+        string azonosito;
+        Szallasfajta szallasfajta;
+
+        public string Azonosito
+        {
+            get => azonosito;
+            set
+            {
+                if (value.Length == 8)
+                {
+                    azonosito = value;
+                }
+                else
+                {
+                    throw new FormatException("Az azonosító 8 karakter hossszú!");
+                }
+            }
+        }
+
+        internal Cim Cim
+        {
+            get => cim;
+            set => cim = value;
+        }
+        internal Szallasfajta Szallasfajta { get => szallasfajta; /*set => szallasfajta = value; */}
+
+
+
+        public override string ToString()
+        {
+            return $"{azonosito}";
+        }
+
+
+
+
+    }
+
+
+}
